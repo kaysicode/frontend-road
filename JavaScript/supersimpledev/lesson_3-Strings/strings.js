3a2b06f4b401fa61789381eb169b69ecdbd4948f
Kaysi <codeceorl@gmail.com>
2025-11-18 07:51:36 +0800

    fix: correct typos in Lesson 2 and add Lesson 3: Strings content

---
+++ JavaScript/supersimpledev/lesson_3-Strings/strings.js
@@ -0,0 +1,11 @@
+// LESSON 3: Strings
+// - In this lesson we will learn basics about strings in JavaScript
+// - In this lesson we can learn that string is a data type that represents text. Strings are used to store and manipulate sequences of characters, such as words, sentences, or any other textual data.
+// - In this lesson we will learn about concatenation, string methods, and template literals.
+
+// LESSON LEARNED:
+// - Strings can be created using single quotes (' '), double quotes (" "), or backticks (` `).
+// - Concatenation is the process of combining strings using the + operator.
+
+
+
