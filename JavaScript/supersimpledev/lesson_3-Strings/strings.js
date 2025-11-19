e8bce5e829630c5e2c64fe046b629120ce9020c7
Kaysi <codeceorl@gmail.com>
2025-11-20 03:36:28 +0800

    feat: finished the lesson 3 in JavaScript

--- JavaScript/supersimpledev/lesson_3-Strings/strings.js
+++ JavaScript/supersimpledev/lesson_3-Strings/strings.js
@@ -1,11 +1,16 @@
-// LESSON 3: Strings
-// - In this lesson we will learn basics about strings in JavaScript
-// - In this lesson we can learn that string is a data type that represents text. Strings are used to store and manipulate sequences of characters, such as words, sentences, or any other textual data.
-// - In this lesson we will learn about concatenation, string methods, and template literals.
+/*
+ LESSON 3: Strings
+ - In this lesson we will learn basics about strings in JavaScript
+ - In this lesson we can learn that string is a data type that represents text. Strings are used to store and manipulate sequences of characters, such as words, sentences, or any other textual data.
+ - In this lesson we will learn about concatenation, string methods, and template literals.
 
-// LESSON LEARNED:
-// - Strings can be created using single quotes (' '), double quotes (" "), or backticks (` `).
-// - Concatenation is the process of combining strings using the + operator.
-
-
-
+ LESSON LEARNED:
+ - Strings can be created using single quotes (' '), double qutes (" "), or backticks (` `).
+ - Concatenation is the process of combining strings using the + operator.
+ - typeof operator can be used to check the data type of a variable.
+ - When calculating a floating number especially when it's a money it's best to use cent when dividing it
+   for example:
+    $19.19 = 1999
+    then divide it by 100 to get the dollar amount
+- Template strings can be multi-line and allow embedded expressions using ${expression} syntax.
+*/
